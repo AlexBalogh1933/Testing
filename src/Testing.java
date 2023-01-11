0d3fa4bbfb439fb54ac85e5bcbb87ed0b2b323a4
Alex <baloghaj@mail.uc.edu>
2023-01-11 17:06:33 -0500

    Test Scanner

--- src/Testing.java
+++ src/Testing.java
@@ -1,8 +1,15 @@
+import java.util.Scanner;
 public class Testing
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        // System.out.println("Hello world!");
 
+        int favNum = 0;
+        Scanner in = new Scanner(System.in);
+
+        favNum = SafeInput.getRangedInt(in, "What is you Fav Num", 1, 10 );
+
+        System.out.println("You said your Fav Num is: " + favNum);
     }
 }
